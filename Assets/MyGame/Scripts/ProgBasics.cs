1e770746533e86e7a835cc7c90805599e4a3960d
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-04-06 12:04:40 +0200

    new script, multiplikative operatoren

---
+++ Assets/MyGame/Scripts/ProgBasics.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgBasics : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("erste Tests mit Operatoren");
+
+        //  Multiplikative Operatoren
+        Debug.Log("Multiplikative Operatoren * " + (4 * 4));
+        Debug.Log("Multiplikative Operatoren / " + (36 / 4));
+        Debug.Log("Multiplikative Operatoren % " + (7 % 2));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
