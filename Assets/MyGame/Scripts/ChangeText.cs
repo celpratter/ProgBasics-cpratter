4dac4ec0ba3a67ac6e67ce51de976d3f865d3db4
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-05-08 21:38:33 +0200

    change textwith script, system.random, unityengine.random

---
+++ Assets/MyGame/Scripts/ChangeText.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    public TestRandom myScript;
+    public Text myText;
+    private int countPresses = 0;
+    public TextMeshProUGUI myTMPText;
+
+    //zufall generieren
+    private System.Random rnd = new System.Random();
+    private System.Random seedRnd = new System.Random(123);
+
+
+    public void addPress()
+    {
+        // countPresses= countPresses + 1;
+        countPresses++;
+        // countPresses += 1;
+        Debug.Log("counts: " + countPresses);
+        myText.text = countPresses.ToString();
+        //mit seed abprüfbar - immer selbe sequenz mit selbem seed
+        Debug.Log("----------- DES IS RANDOM seed " + seedRnd.Next(0, 5));
+
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //variable.typ
+        myText.text = "Helloo";
+        myText.fontSize = 200;
+        myTMPText.text = "byeee";
+        Debug.Log("+++++++ DES IS RANDOM " + rnd.Next(0, 5));
+        Debug.Log("||||||| DES IS RANDOM " + UnityEngine.Random.Range(0, 1));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/MyGame/Scripts/TestRandom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRandom : MonoBehaviour
+{
+    public float minValX = -6f;
+    public float maxValX = 6f;
+    public float rndVal;
+    public float rndVal2;
+    private System.Random rndSystem = new System.Random();
+
+    void Start()
+    {
+        rndVal = rndSystem.Next(-6, 6);
+        rndVal2 = UnityEngine.Random.Range(4, 100);
+        Debug.Log("--- TEST RANDOM --- SYSTEM --- " + rndVal);
+        Debug.Log("--- TEST RANDOM --- UE --- " + rndVal2);
+        
+    }
+
+	public void ChangeRandomCube()
+	{
+		gameObject.transform.position = new Vector3(rndVal, 0f, 0f);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
